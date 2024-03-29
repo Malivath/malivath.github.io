a3a2ee4dc1e6974d302a3036547a3c1bbbc0eaed
Malivath <eavilcows13@hotmail.com>
2024-03-28 21:46:32 -0500

    Updated navbar, fixed the flashing problem with the changing background

--- paintingresto/script.js
+++ paintingresto/script.js
@@ -1,11 +1,27 @@
 let imageIndex = 1;
+let images = [];
+
+function preloadImages() {
+    // Create a hidden div
+    const hiddenDiv = document.createElement('div');
+    hiddenDiv.style.display = 'none';
+    document.body.appendChild(hiddenDiv);
+
+    for (let i = 1; i <= 4; i++) {
+        images[i] = new Image();
+        images[i].src = `public/${i}.jpg`;
+
+        // Add the image to the hidden div
+        hiddenDiv.appendChild(images[i]);
+    }
+}
 
 function changeBackgroundImage() {
     // Get the body element
     const body = document.querySelector('body');
 
     // Set the background image
-    body.style.backgroundImage = `url('public/${imageIndex}.jpg')`;
+    body.style.backgroundImage = `url('${images[imageIndex].src}')`;
 
     // Increment the image index
     imageIndex++;
@@ -15,6 +31,9 @@ function changeBackgroundImage() {
         imageIndex = 1;
     }
 }
+
+// Preload the images
+preloadImages();
 
 // Call the function every 5 seconds (15000 milliseconds)
 setInterval(changeBackgroundImage, 15000);
